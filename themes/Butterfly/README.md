0c7b6b3b61fab1c516538bebc8f2ead0faa8c449
iseki <admin@iseki.space>
2020-09-06 18:30:42 +0800

    a

--- themes/Butterfly/README.md
+++ themes/Butterfly/README.md
@@ -1,37 +1,96 @@
 # hexo-theme-butterfly
 
-[预览](https://jerryc.me/)
+![version](https://img.shields.io/github/package-json/v/jerryc127/hexo-theme-butterfly)
+![https://img.shields.io/npm/v/hexo-theme-butterfly?color=%09%23bf00ff](https://img.shields.io/npm/v/hexo-theme-butterfly?color=%09%23bf00ff)
+![hexo version](https://img.shields.io/badge/hexo-4.2+-0e83c)
+![npm download](https://img.shields.io/npm/dw/hexo-theme-butterfly?color=green)
+![license](https://img.shields.io/github/license/jerryc127/hexo-theme-butterfly?color=FF5531)
 
-一款基于[hexo-theme-melody](https://github.com/Molunerfinn/hexo-theme-melody)修改的主题
+Demo: 👍 [Butterfly](https://demo.jerryc.me/)  ||   🤞 [JerryC](https://jerryc.me/)
 
-## 安装
+Docs: 📖 [Butterfly Docs](https://demo.jerryc.me/posts/21cfbf15/)
 
-在你的博客根目录里
+Based on [hexo-theme-melody](https://github.com/Molunerfinn/hexo-theme-melody) theme.
 
-```
-git clone -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/Butterfly
-```
+## Installation
 
-如果想要安裝比較新的dev分支，可以
+### GIT
 
-```
-git clone -b dev https://github.com/jerryc127/hexo-theme-butterfly.git themes/Butterfly
-```
-
-## 应用主题
-修改hexo配置文件`_config.yml`，把主题改为`Butterfly`
+Stable branch [recommend]:
 
 ```
-theme: Butterfly
+git clone -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
 ```
 
->如果你没有pug以及stylus的渲染器，请下载安装： npm install hexo-renderer-jade hexo-renderer-stylus --save or yarn add hexo-renderer-jade hexo-renderer-stylus
+Dev branch:
 
-## 文档
+```
+git clone -b dev https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
+```
 
-可查看或[这里](https://docs.jerryc.me/)或[这里](https://jerryc.me/posts/21cfbf15)
+### NPM
 
+> It supports Hexo 5.0.0 or later
 
-## 截图
-![image](https://user-images.githubusercontent.com/16351105/58887365-1272f780-8718-11e9-9329-3292c6ba20d4.png)
-![image](https://user-images.githubusercontent.com/16351105/58887457-3cc4b500-8718-11e9-9417-2bdea603c92e.png)
+In Hexo site root directory 
+
+```powershell
+npm i hexo-theme-butterfly
+```
+
+## Configuration
+
+ Set theme in the hexo work folder's root config file `_config.yml`: 
+
+> theme: butterfly
+
+ If you don't have pug & stylus renderer, try this: 
+
+> npm install hexo-renderer-pug hexo-renderer-stylus
+
+## Features
+
+- [x] Card UI Design
+- [X] Support sub-menu
+- [x] Two Column designs
+- [x] Responsive Web Design
+- [x] Dark Mode
+- [x] Pjax
+- [x] Read Mode
+- [x] Conversion between Traditional and Simplified Chinese
+- [X] TOC catalog is available for both computers and mobile phones
+- [X] Color themes (darker/pale night/light/ocean/mac/mac light), support custom colors
+- [X] Code Blocks (Display code language/close or expand Code Blocks/Copy Button/word wrap)
+- [X] Disable copy/Add a Copyright Notice to the Copied Text
+- [X] Search (Algolia SearchZ/Local Search)
+- [x] Mathjax and Katex
+- [x] Built-in 404 page
+- [x] WordCount
+- [x] Related articles
+- [x] Displays outdated notice for a post
+- [x] Share (AddThis/Sharejs/Addtoany)
+- [X] Comment (Disqus/Disqusjs/Livere/Gitalk/Valine/Utterances/Facebook Comments)
+- [x] Multiple Comment System Support
+- [x] Online Chats (Chatra/Tidio/Daovoice/Gitter/Crisp)
+- [x] Web analytics (Baidu Analytics/Google Analytics/Tencent Analytics/CNZZ Analytics)
+- [x] Google AdSense
+- [x] Webmaster Verification (google/Bing/Baidu/360/Yandex)
+- [x] Change website colour scheme
+- [x] Typewriter Effect: activate_power_mode
+- [x] Background effects (Canvas ribbon/canvas_ribbon_piao/canvas_nest)
+- [x] Mouse click effects (Fireworks/Heart/Text)
+- [x] Preloader/Loading Animation
+- [x] Busuanzi visitor counter
+- [x] Medium Zoom/Fancybox
+- [x] Mermaid
+- [x] Justified Gallery
+- [x] Lazyload images
+- [x] Instantpage/Pangu/Snackbar notification toast/PWA......
+
+## Screenshots
+
+![](https://cdn.jsdelivr.net/gh/jerryc127/CDN/img/theme-butterfly-readme-homepage-1.png)
+
+![](https://cdn.jsdelivr.net/gh/jerryc127/CDN/img/theme-butterfly-readme-homepage-2.png)
+
+![](https://cdn.jsdelivr.net/gh/jerryc127/CDN/img/theme-butterfly-readme-post.png)
--- themes/Butterfly/source/js/search/algolia.js
+++ themes/Butterfly/source/js/search/algolia.js
@@ -1,47 +1,53 @@
 $(function () {
-  $('a.social-icon.search').on('click', function () {
-    $('body').css('width', '100%')
-    $('body').css('overflow', 'hidden')
-
-    $('.search-dialog').animate({}, function () {
-      $('.search-dialog').css({
-        'display': 'block',
-        'animation': 'titlescale 0.5s'
-      }),300
-    })
-
+  const openSearch = () => {
+    $('body').css({ width: '100%', overflow: 'hidden' })
+    $('#algolia-search').css('display', 'block')
     $('.ais-search-box--input').focus()
-    $('.search-mask').fadeIn();
+    $('#search-mask').fadeIn()
     // shortcut: ESC
-    document.addEventListener('keydown', function f(event) {
-      if (event.code == "Escape") {
-        closeSearch();
-        document.removeEventListener('keydown', f);
+    document.addEventListener('keydown', function f (event) {
+      if (event.code === 'Escape') {
+        closeSearch()
+        document.removeEventListener('keydown', f)
       }
     })
-  })
+  }
 
-  var closeSearch = function () {
-    $('body').css('overflow', 'auto')
-
-    $('.search-dialog').animate({}, function () {
-      $('.search-dialog').css({
-        'display': 'none'
-      })
+  const closeSearch = () => {
+    $('body').css({ width: '', overflow: '' })
+    $('#algolia-search').css({
+      animation: 'search_close .5s'
     })
 
-    $('.search-mask').fadeOut();
+    setTimeout(function () {
+      $('#algolia-search').css({
+        animation: '',
+        display: 'none'
+      })
+    }, 500)
+
+    $('#search-mask').fadeOut()
   }
-  $('.search-mask, .search-close-button').on('click', closeSearch)
 
+  const searchClickFn = () => {
+    $('a.social-icon.search').on('click', openSearch)
+    $('#search-mask, .search-close-button').on('click touchstart', closeSearch)
+  }
 
-  var algolia = GLOBAL_CONFIG.algolia
-  var isAlgoliaValid = algolia.appId && algolia.apiKey && algolia.indexName
+  searchClickFn()
+
+  window.addEventListener('pjax:success', function () {
+    closeSearch()
+    searchClickFn()
+  })
+
+  const algolia = GLOBAL_CONFIG.algolia
+  const isAlgoliaValid = algolia.appId && algolia.apiKey && algolia.indexName
   if (!isAlgoliaValid) {
     return console.error('Algolia setting is invalid!')
   }
 
-  var search = instantsearch({
+  const search = instantsearch({
     appId: algolia.appId,
     apiKey: algolia.apiKey,
     indexName: algolia.indexName,
@@ -49,7 +55,7 @@ $(function () {
       hitsPerPage: algolia.hits.per_page || 10
     },
     searchFunction: function (helper) {
-      var searchInput = $('#algolia-search-input').find('input')
+      const searchInput = $('#algolia-search-input').find('input')
 
       if (searchInput.val()) {
         helper.search()
@@ -70,7 +76,7 @@ $(function () {
       container: '#algolia-hits',
       templates: {
         item: function (data) {
-          var link = data.permalink ? data.permalink : (GLOBAL_CONFIG.root + data.path)
+          const link = data.permalink ? data.permalink : (GLOBAL_CONFIG.root + data.path)
           return (
             '<a href="' + link + '" class="algolia-hit-item-link">' +
             data._highlightResult.title.value +
@@ -96,7 +102,7 @@ $(function () {
       container: '#algolia-stats',
       templates: {
         body: function (data) {
-          var stats = GLOBAL_CONFIG.algolia.languages.hits_stats
+          const stats = GLOBAL_CONFIG.algolia.languages.hits_stats
             .replace(/\$\{hits}/, data.nbHits)
             .replace(/\$\{time}/, data.processingTimeMS)
           return (
@@ -117,10 +123,10 @@ $(function () {
       scrollTo: false,
       showFirstLast: false,
       labels: {
-        first: '<i class="fa fa-angle-double-left"></i>',
-        last: '<i class="fa fa-angle-double-right"></i>',
-        previous: '<i class="fa fa-angle-left"></i>',
-        next: '<i class="fa fa-angle-right"></i>'
+        first: '<i class="fas fa-angle-double-left"></i>',
+        last: '<i class="fas fa-angle-double-right"></i>',
+        previous: '<i class="fas fa-angle-left"></i>',
+        next: '<i class="fas fa-angle-right"></i>'
       },
       cssClasses: {
         root: 'pagination',
@@ -131,6 +137,9 @@ $(function () {
       }
     })
   )
-
   search.start()
+
+  window.pjax && search.on('render', () => {
+    window.pjax.refresh(document.getElementById('algolia-hits'))
+  })
 })
